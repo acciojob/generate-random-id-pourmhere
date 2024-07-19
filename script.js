d436d274389cecaa26752b990a2f5c9510ba00fc
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-19 17:06:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+
+    for (let i = 0; i < l; i++) {
+        // Generate a random index to select a character from the characters string
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        // Append the selected character to the result string
+        result += characters.charAt(randomIndex);
+    }
+
+    return result; 
 }
 
 // Do not change the code below.
